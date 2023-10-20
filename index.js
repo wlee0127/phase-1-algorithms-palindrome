1ae5e3347d6f50d7c504d9841f6fff3e3ed16f44
william lee <will.lee.npc@gmail.com>
2023-10-20 00:47:42 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  result = "";
+  for(i=word.length-1; i>=0; i--){
+    result = result.concat(word.charAt(i));
+    console.log(result);
+  }
+  if(result===word){
+    return true;
+  }else{
+    return false;
+  }
 }
+
+isPalindrome("racecar")
 
 /* 
   Add your pseudocode here
+  let variable result hold resulting string;
+  loop through input string
+    for i=length.input string - 1, iterate backwards
+    result=result concat inputstring[i]
+  
+    test if result === input string.
+              return true
+        else return false
+  
 */
 
 /*
